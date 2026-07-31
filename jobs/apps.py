7d930744f1a2b9131017e482cd41db092a43f5a4
lamboe69 <erickojok69@gmail.com>
2026-07-31 10:22:58 +0300

    fix: sqlite 'database is locked' from background job aggregation
    Enable WAL journal mode and a 20s busy timeout on sqlite connections so
    concurrent requests don't fail while the job aggregation scheduler
    writes in the background.

--- jobs/apps.py
+++ jobs/apps.py
@@ -1,6 +1,17 @@
 import os
 
 from django.apps import AppConfig
+from django.db.backends.signals import connection_created
+
+
+def _enable_sqlite_concurrency(sender, connection, **kwargs):
+    if connection.vendor == 'sqlite':
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute('PRAGMA journal_mode=WAL;')
+                cursor.execute('PRAGMA busy_timeout=20000;')
+        except Exception:
+            pass
 
 
 class JobsConfig(AppConfig):
@@ -9,6 +20,7 @@ class JobsConfig(AppConfig):
     verbose_name = 'Jobs'
 
     def ready(self):
+        connection_created.connect(_enable_sqlite_concurrency, dispatch_uid='jobs_enable_sqlite_concurrency')
         if os.environ.get('RUN_MAIN') == 'true':
             from .scheduler import start_scheduler
             start_scheduler()
